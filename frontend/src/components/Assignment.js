1f40325eb22d9853c4ae12de5dc9b3d17bf5c511
Mursal Mousumi <mursalm@yahoo.com>
2022-10-11 20:54:01 -0700

    add new assignment

--- frontend/src/components/Assignment.js
+++ frontend/src/components/Assignment.js
@@ -9,7 +9,7 @@ import {DataGrid} from '@mui/x-data-grid';
 import {SERVER_URL} from '../constants.js'
 
 // NOTE:  for OAuth security, http request must have
-//   credentials: 'include' 
+//   credentials: 'include'
 //
 
 class Assignment extends React.Component {
@@ -17,38 +17,70 @@ class Assignment extends React.Component {
       super(props);
       this.state = {selected: 0, assignments: []};
     };
- 
+
    componentDidMount() {
     this.fetchAssignments();
   }
- 
+
+  newAssignment = (assignment) => {
+  const token = Cookies.get('XSRF-TOKEN');
+  fetch(`${SERVER_URL}/assignment`,
+    {
+      method: 'POST',
+      headers:
+      {
+        'X-XSRF-TOKEN': token,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(assignment)
+    } )
+  .then(res => {
+      if (res.ok) {
+        toast.success("Assignment successfully added", {
+            position: toast.POSITION.BOTTOM_LEFT
+        });
+        this.fetchAssignments();
+      } else {
+        toast.error("Error new assignment failed.", {
+            position: toast.POSITION.BOTTOM_LEFT
+        });
+        console.error('Post http status =' + res.status);
+      }})
+  .catch(err => {
+    toast.error("Error new assignment failed.", {
+          position: toast.POSITION.BOTTOM_LEFT
+      });
+      console.error(err);
+  })
+} 
+
   fetchAssignments = () => {
     console.log("Assignment.fetchAssignments");
     const token = Cookies.get('XSRF-TOKEN');
-    fetch(`${SERVER_URL}/gradebook`, 
-      {  
-        method: 'GET', 
+    fetch(`${SERVER_URL}/gradebook`,
+      {
+        method: 'GET',
         headers: { 'X-XSRF-TOKEN': token }
       } )
-    .then((response) => response.json()) 
-    .then((responseData) => { 
+    .then((response) => response.json())
+    .then((responseData) => {
       if (Array.isArray(responseData.assignments)) {
-        //  add to each assignment an "id"  This is required by DataGrid  "id" is the row index in the data grid table 
+        //  add to each assignment an "id"  This is required by DataGrid  "id" is the row index in the data grid table
         this.setState({ assignments: responseData.assignments.map((assignment, index) => ( { id: index, ...assignment } )) });
       } else {
         toast.error("Fetch failed.", {
           position: toast.POSITION.BOTTOM_LEFT
         });
-      }        
+      }
     })
-    .catch(err => console.error(err)); 
+    .catch(err => console.error(err));
   }
-  
+
    onRadioClick = (event) => {
     console.log("Assignment.onRadioClick " + event.target.value);
     this.setState({selected: event.target.value});
   }
-  
+
   render() {
      const columns = [
       {
@@ -71,22 +103,23 @@ class Assignment extends React.Component {
       { field: 'courseTitle', headerName: 'Course', width: 300 },
       { field: 'dueDate', headerName: 'Due Date', width: 200 }
       ];
-      
+
       const assignmentSelected = this.state.assignments[this.state.selected];
       return (
           <div align="left" >
             <h4>Assignment(s) ready to grade: </h4>
               <div style={{ height: 450, width: '100%', align:"left"   }}>
                 <DataGrid rows={this.state.assignments} columns={columns} />
-              </div>                
-            <Button component={Link} to={{pathname:'/gradebook',   assignment: assignmentSelected }} 
+              </div>
+            <Button component={Link} to={{pathname:'/gradebook',   assignment: assignmentSelected }}
                     variant="outlined" color="primary" disabled={this.state.assignments.length===0}  style={{margin: 10}}>
               Grade
             </Button>
-            <ToastContainer autoClose={1500} /> 
+            <AddAssignment  add={this.newAssignment} />
+            <ToastContainer autoClose={1500} />
           </div>
       )
   }
-}  
+}
 
 export default Assignment;
